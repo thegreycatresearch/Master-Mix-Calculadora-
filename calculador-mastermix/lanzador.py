4b9a3423a719ef3c8163fb654cb5f4ada510b683
The Grey Cat - RESEARCH <krisstazzone@gmail.com>
2026-05-29 19:12:56 -0300

    Update lanzador.py

--- calculador-mastermix/lanzador.py
+++ calculador-mastermix/lanzador.py
@@ -2,14 +2,20 @@ import os
 import sys
 import streamlit.web.cli as stcli
 
-# 1. Bloquea el cartel molesto del email de raíz
+# 1. Bloquea la solicitud de email de Streamlit para cualquier usuario
 os.environ["STREAMLIT_BROWSER_GATHER_USAGE_STATS"] = "false"
 
 if __name__ == "__main__":
-    # 2. Si es el .exe, lo obliga a pararse en su carpeta temporal para no perder los archivos
+    # 2. Si se ejecuta como .exe, obliga al programa a pararse en su carpeta temporal
     if hasattr(sys, '_MEIPASS'):
         os.chdir(sys._MEIPASS)
+        sys.path.append(sys._MEIPASS)
+    else:
+        # Si se ejecuta en modo desarrollo local
+        ruta_actual = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(ruta_actual)
+        sys.path.append(ruta_actual)
     
-    # 3. Arranca Streamlit buscando 'app_web.py' directamente en el lugar correcto
+    # 3. Inicia Streamlit buscando 'app_web.py' directamente en el lugar correcto
     sys.argv = ["streamlit", "run", "app_web.py", "--global.developmentMode=false"]
     sys.exit(stcli.main())
